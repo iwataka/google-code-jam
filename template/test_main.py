6803f1a2a1065e0ebdc2574644959bf0d543ba9c
iwataka <ap.gr.le@gmail.com>
2018-04-13 22:33:37 +0900

    Remove some lines from a template test file

--- template/test_main.py
+++ template/test_main.py
@@ -6,11 +6,8 @@ from main import *
 class TestMain(unittest.TestCase):
 
     def test_solve(self):
-        # TODO: Edit this test freely
         ans = solve()
         self.assertEqual(ans, 0)
-
-    # TODO: Write your own tests here
 
 if __name__ == '__main__':
     unittest.main()
