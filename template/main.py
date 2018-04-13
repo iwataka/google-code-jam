2c17172c85c2f1f89c2fc36c2e029ebdbd431e67
iwataka <ap.gr.le@gmail.com>
2018-04-13 22:31:38 +0900

    Add more lines to a template file

--- template/main.py
+++ template/main.py
@@ -8,5 +8,7 @@ if __name__ == '__main__':
     n_tests = int(input())
     for i in range(n_tests):
         # TODO: Get more inputs per test case
+        # For example:
+        # xs = [int(x) for x in input().split()]
         ans = solve()
         print("Case #%d: %d" % (i + 1, ans))
