2e06397b957c64286e75998b5b60e993bb3b6c82
iwataka <ap.gr.le@gmail.com>
2019-03-05 03:13:16 +0900

    Complete bit party problem

--- 2018/round1a/bit_party/main.py
+++ 2018/round1a/bit_party/main.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
 
-import math
-
 def solve(n_robots, n_bits, cashier_infos):
     max_spent_time = max([x[0] * x[1] + x[2] for x in cashier_infos])
     smallest_time = 0
     largetst_time = max_spent_time
     while smallest_time < largetst_time:
         time = (smallest_time + largetst_time) // 2
-        if able_to_buy(n_robots, n_bits, cashier_infos, time):
+        if is_purchasable(n_robots, n_bits, cashier_infos, time):
             largetst_time = time
         else:
             smallest_time = time + 1
     return smallest_time
 
-def able_to_buy(n_robots, n_bits, cashier_infos, time):
-    capacities = [max(0, min(x[0], math.floor((time - x[2]))) / x[1]) for x in cashier_infos]
-    return n_bits <= sum(capacities[0:n_robots + 1])
+
+def is_purchasable(n_robots, n_bits, cashier_infos, time):
+    bit_capacities_per_cashier = []
+    for cashier_info in cashier_infos:
+        time_to_scan = time - cashier_info[2]
+        n_scannable_items = time_to_scan // cashier_info[1]
+        n_capable_items = cashier_info[0]
+        bit_capacities_per_cashier.append(max(0, min(n_capable_items, n_scannable_items)))
+    bit_capacities_per_cashier.sort(reverse=True)
+    return n_bits <= sum(bit_capacities_per_cashier[0:n_robots])
+
 
 if __name__ == '__main__':
     n_tests = int(input())
