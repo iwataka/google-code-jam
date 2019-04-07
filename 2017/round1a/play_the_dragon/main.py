92868ba07d8ff775cd0515d8973e312a17c7f714
iwataka <ap.gr.le@gmail.com>
2019-04-07 18:01:35 +0900

    temp commit

--- 2017/round1a/play_the_dragon/main.py
+++ 2017/round1a/play_the_dragon/main.py
@@ -10,7 +10,7 @@ def solve(Hd, Ad, Hk, Ak, B, D):
         Ak: the knight's attack.
         B: buff value.
         D: debuff value.
-    
+
     Returns:
         The minimum number of turns to beat the knight.
 
@@ -34,6 +34,7 @@ def solve(Hd, Ad, Hk, Ak, B, D):
 
     n_turns_for_attacks_and_buffs = optimize_attacks_and_buffs(Ad, Hk, B)
     return optimize(Hd, Ak, D, n_turns_for_attacks_and_buffs)
+
 
 def optimize_attacks_and_buffs(Ad, Hk, B):
     """Calculates the minimum numbers of turns taken only for attacks and buffs.
@@ -63,6 +64,7 @@ def optimize_attacks_and_buffs(Ad, Hk, B):
         n_buffs += 1
     return min_n_turns
 
+
 def optimize(Hd, Ak, D, n_turns_for_attacks_and_buffs):
     """Calculates the minimum number of turns taken to beat the knight.
 
@@ -88,6 +90,7 @@ def optimize(Hd, Ak, D, n_turns_for_attacks_and_buffs):
     while True:
         # No meanings to debuff any more.
         if Ak == 0:
+            # No need to cure because the knight's attack is zero.
             n_turns = n_turns_for_debuffs_and_cures + n_turns_for_attacks_and_buffs
             return min(min_n_turns, n_turns)
 
@@ -95,20 +98,69 @@ def optimize(Hd, Ak, D, n_turns_for_attacks_and_buffs):
         # Calculates the total number of turns if required.
         if interval_between_cures > max_interval_between_cures:
             max_interval_between_cures = interval_between_cures
-            n_turns = n_turns_for_debuffs_and_cures + optimize_cures(Hd, Hd_orig, Ak, n_turns_for_attacks_and_buffs)
+            n_turns = n_turns_for_debuffs_and_cures + \
+                optimize_cures(Hd, Hd_orig, Ak, n_turns_for_attacks_and_buffs)
             min_n_turns = min(min_n_turns, n_turns)
 
-        # Cure if needed.
+        # Cure if needed and debuff iteratively.
         if Hd <= Ak - D:
-            Hd = Hd_orig - Ak
+            Hd = Hd_orig - Ak  # Cure
             n_turns_for_debuffs_and_cures += 1
 
-        # Debuff.
-        Ak = max(0, Ak - D)
-        Hd -= Ak
-        n_turns_for_debuffs_and_cures += 1
+            next_max_Ak = calc_next_max_Ak(
+                Ak, interval_between_cures, Hd_orig, D)
+            if not math.ceil(Hd_orig / (next_max_Ak + D)) - 2 == interval_between_cures:
+                raise Exception(Hd_orig, next_max_Ak, D,
+                                interval_between_cures)
+            if not math.ceil(Hd_orig / next_max_Ak) - 2 > interval_between_cures:
+                raise Exception(Hd_orig, next_max_Ak, D,
+                                interval_between_cures)
+            n_debuffs_until_next_interval = (Ak - next_max_Ak) // D
+            n_repeats = (n_debuffs_until_next_interval - 1) // interval_between_cures
+
+            Ak -= D * interval_between_cures * n_repeats
+            n_turns_for_debuffs_and_cures += n_repeats * \
+                (interval_between_cures + 1)
+            Hd = Hd_orig - Ak
+        else:
+            # Debuff.
+            Ak = max(0, Ak - D)
+            Hd -= Ak
+            n_turns_for_debuffs_and_cures += 1
 
     return min_n_turns
+
+
+def calc_next_max_Ak(Ak, interval_between_cures, Hd_orig, D):
+    # interval_between_cures + 2 = math.ceil(Hd_orig / Ak)
+    # Hd_orig / Ak - 1 <= interval + 2 < Hd_orig / Ak
+    #
+    # (interval + 2) * Ak < Hd_orig
+    # Ak * (interval + 2) / (interval + 3) < Hd_orig / (interval + 3)
+    # math.ceil(Ak * (interval + 2) / (interval + 3)) <= math.ceil(Hd_orig / (interval + 3))
+    # math.ceil(Ak * (interval + 2) / (interval + 3)) <= next_max_Ak
+    #
+    # Hd_orig <= (interval + 3) * Ak
+    # Hd_orig / (interval + 3) <= Ak
+    # math.ceil(Hd_orig / (interval + 3)) <= math.ceil(Ak)
+    # next_max_Ak <= math.ceil(Ak) = Ak
+    lowest = math.ceil(Ak * (interval_between_cures + 2) /
+                       (interval_between_cures + 3))
+    highest = Ak
+    while True:
+        mid = (lowest + highest) // 2
+        tmp_interval = math.ceil(Hd_orig / mid) - 2
+        if interval_between_cures == tmp_interval:
+            highest = mid
+        else:
+            lowest = mid
+        if highest - lowest < 10 * D:
+            break
+    next_max_Ak = Ak - ((Ak - highest) // D) * D
+    while math.ceil(Hd_orig / next_max_Ak) - 2 == interval_between_cures:
+        next_max_Ak -= D
+    return next_max_Ak
+
 
 def optimize_cures(Hd, Hd_orig, Ak, n_turns_for_attacks_and_buffs):
     """
@@ -124,10 +176,12 @@ def optimize_cures(Hd, Hd_orig, Ak, n_turns_for_attacks_and_buffs):
     interval = math.ceil(Hd_orig / Ak) - 2
     if interval == 0:
         return math.pow(10, 9) * 2
-    n_intervals = math.floor((n_turns_for_attacks_and_buffs - n_turns_until_first_attacks) / interval)
+    n_intervals = math.floor(
+        (n_turns_for_attacks_and_buffs - n_turns_until_first_attacks) / interval)
     if interval == 1:
         return n_turns_until_first_attacks + (interval + 1) * (n_intervals - 1) + 1
-    mod = (n_turns_for_attacks_and_buffs - n_turns_until_first_attacks) % interval
+    mod = (n_turns_for_attacks_and_buffs -
+           n_turns_until_first_attacks) % interval
     if mod == 1:
         return n_turns_until_first_attacks + (interval + 1) * n_intervals + 1
     elif mod == 0:
