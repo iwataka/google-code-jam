36aac18daa3a1dee556596f2f2148c2d01570151
iwataka <ap.gr.le@gmail.com>
2019-08-25 20:14:20 +0900

    add python code

--- 2018/round1b/mysterious_road_signs/main.py
+++ 2018/round1b/mysterious_road_signs/main.py
@@ -1,112 +1,60 @@
 #!/usr/bin/env python
 
-
-def solve(signs):
-    queue = [(0, len(signs) - 1)]
-    max_size = 0
-    max_count = 0
-    # TODO: priority queue
-    while queue:
-        start_i, end_i = queue.pop()
-        if end_i - start_i + 1 < max_size:
-            continue
-        size, count = longetst_path_from_center(signs, start_i, end_i)
-        max_size, max_count = append_size(max_size, max_count, size, count)
-        mid_i = (start_i + end_i) // 2
-        if start_i <= mid_i - 1:
-            queue.append((start_i, mid_i - 1))
-        if mid_i + 1 <= end_i:
-            queue.append((mid_i + 1, end_i))
-    return max_size, max_count
+import itertools
 
 
-def longetst_path_from_center(signs, s, e):
-    mid_i = (s + e) // 2
-    subseq_list = []
-    subseq_list.append(walkthrough(signs, mid_i, s, 0))
-    subseq_list.append(walkthrough(signs, mid_i, e, 0))
-    subseq_list.append(walkthrough(signs, mid_i, s, 1))
-    subseq_list.append(walkthrough(signs, mid_i, e, 1))
+def reverse_sign(sign):
+    return (sign[0], sign[2], sign[1])
 
-    nums2range = {}
-    for M, N, start, end in subseq_list:
-        if not (M, N) in nums2range:
-            nums2range[(M, N)] = (start, end)
+
+def span_contiguous_signs(signs):
+    m = signs[0][1]
+    contiguous_signs, rest_signs = span(lambda s: s[1] == m, signs)
+    if rest_signs:
+        n = rest_signs[0][2]
+        contiguous_signs += list(itertools.takewhile(lambda s: s[1] == m or s[2] == n, rest_signs))
+    return contiguous_signs, rest_signs
+
+
+def span(pred, itr):
+    front = []
+    rear = []
+    append_front = True
+    for x in itr:
+        if not pred(x):
+            append_front = False
+        if append_front:
+            front.append(x)
         else:
-            cur_start, cur_end = nums2range[(M, N)]
-            new_start = start if start < cur_start else cur_start
-            new_end = end if end > cur_end else cur_end
-            nums2range[(M, N)] = (new_start, new_end)
-
-    sizes = map(lambda x: x[1] - x[0] + 1, set(nums2range.values()))
-
-    max_size = 0
-    max_count = 0
-    for size in sizes:
-        max_size, max_count = append_size(max_size, max_count, size, 1)
-    return max_size, max_count
+            rear.append(x)
+    return front, rear
 
 
-def append_size(cur_max_size, cur_max_count, size, count):
-    if size > cur_max_size:
-        return size, count
-    elif size == cur_max_size:
-        return size, cur_max_count + count
-    else:
-        return cur_max_size, cur_max_count
+def to_range(contiguous_signs):
+    return (contiguous_signs[0][0], contiguous_signs[-1][0])
 
 
-def walkthrough(signs, i_start, i_end, i_fix):
-    if i_start == i_end:
-        return signs[i_start][0], signs[i_start][1], i_start, i_start
-
-    i_not_fix = 0 if i_fix else 1
-    fix_val = signs[i_start][i_fix]
-    not_fix_val = None
-    i_subseq_end = None
-    direction = 1 if i_end > i_start else -1
-
-    for i in range(i_start + direction, i_end + direction, direction):
-        i_subseq_end = i
-        sign = signs[i]
-        if sign[i_fix] == fix_val:
-            continue
-        if not_fix_val is None:
-            not_fix_val = sign[i_not_fix]
-            continue
-        if sign[i_not_fix] == not_fix_val:
-            continue
-        i_subseq_end = i - direction
-        break
-    if not_fix_val is None:
-        not_fix_val = signs[i_start][i_not_fix]
-
-    if i_fix == 0:
-        M = fix_val
-        N = not_fix_val
-    else:
-        M = not_fix_val
-        N = fix_val
-    if i_start < i_subseq_end:
-        start = i_start
-        end = i_subseq_end
-    else:
-        start = i_subseq_end
-        end = i_start
-    return M, N, start, end
+def signs_to_contiguous_ranges(signs):
+    rest_signs = signs
+    ranges = []
+    while (rest_signs):
+        contiguous_signs, rest_signs = span_contiguous_signs(rest_signs)
+        ranges.append(to_range(contiguous_signs))
+    return ranges
 
 
-def solve_all():
-    n_tests = int(input())
-    for i in range(n_tests):
-        n_signs = int(input())
-        signs = []
-        for _ in range(n_signs):
-            d, a, b = map(int, input().split())
-            signs.append((d + a, d - b))
-        max_num, max_size = solve(signs)
-        print("Case #%d: %d %d" % (i + 1, max_num, max_size))
+def size(rng):
+    return rng[1] - rng[0] + 1
 
 
-if __name__ == '__main__':
-    solve_all()
+nTests = int(input())
+for i in range(nTests):
+    nSigns = int(input())
+    signs = []
+    for j in range(nSigns):
+        d, a, b = map(int, input().split())
+        signs.append((j, d + a, d - b))
+    contiguous_ranges = signs_to_contiguous_ranges(signs) + signs_to_contiguous_ranges(list(map(reverse_sign, signs)))
+    max_size = max(map(size, contiguous_ranges))
+    count = len(set(filter(lambda r: size(r) == max_size, contiguous_ranges)))
+    print("Case #%d: %d %d" % (i + 1, max_size, count))
