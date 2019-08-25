8080a3c6bcf0a99431d60f5d3675077d0cad4eea
iwataka <ap.gr.le@gmail.com>
2019-08-25 19:06:37 +0900

    solve mysterious road signs (Test set 1 only)

--- 2018/round1b/mysterious_road_signs/main.hs
+++ 2018/round1b/mysterious_road_signs/main.hs
@@ -1,53 +1,39 @@
 import qualified Control.Monad as Monad
-import qualified Data.Maybe    as Maybe
 import qualified Data.Set      as Set
 
-splitIntoRanges l = let gaps = extractGaps (zip [0..] l)
-                        gaps' = 0 : gaps ++ [length l - 1]
-                     in listToRanges gaps'
+data Sign = Sign { index :: Int
+                 , m     :: Int
+                 , n     :: Int
+                 } deriving (Show)
 
-listToRanges []             = []
-listToRanges (a : c : rest) = (a, c) : listToRanges rest
+reverseSign (Sign index m n) = Sign index n m
 
-extractGaps [] = []
-extractGaps [_] = []
-extractGaps ((a, b) : (c, d) : rest)
-  | b == d = extractGaps ((c, d) : rest)
-  | otherwise = a : c : extractGaps ((c, d) : rest)
+spanContiguousSigns signs = let headM = m $ head signs
+                                (signsSameAsHeadM, restSigns) = span ((== headM) . m) signs
+                                headN = n $ head restSigns
+                                isContiguous sign = m sign == headM || n sign == headN
+                                contiguousSigns = signsSameAsHeadM ++ takeWhile isContiguous restSigns
+                             in (contiguousSigns, restSigns)
 
-mergeTwoRanges (s1, e1) (s2, e2) = (min s1 s2, max e1 e2)
+toRange contiguousSigns = (index $ head contiguousSigns, index $ last contiguousSigns)
 
-spanMergeable (s, e) rs = let (mergeables, usedAfter) = span ((< e) . snd) rs
-                              mergeables' = takeWhile ((<= e + 1) . fst) usedAfter
-                           in (mergeables ++ mergeables', usedAfter)
+signsToContiguousRanges signs = let (contiguousSigns, restSigns) = spanContiguousSigns signs
+                                    range = toRange contiguousSigns
+                                 in if null restSigns then [range]
+                                                      else range : signsToContiguousRanges restSigns
 
-merge' r rs = let (mergeables, usedAfter) = spanMergeable r rs
-               in (map (mergeTwoRanges r) mergeables, usedAfter)
-
-merge [] rs = []
-merge (r : rest) rs = let (mergedRanges, usedAfter) = merge' r rs
-                       in mergedRanges ++ merge rest usedAfter
+lineToSign index line = let [d, a, b] = words line
+                         in Sign index (read d + read a) (read d - read b)
 
 size (start, end) = end - start + 1
 
-solve ms ns = let mRanges = splitIntoRanges ms
-                  nRanges = splitIntoRanges ns
-                  mergedRanges = merge mRanges nRanges
-                  maxSize = maximum $ map size mergedRanges
-                  count = Set.size $ Set.fromList $ filter ((== maxSize) . size) mergedRanges
-               in (maxSize, count)
-
-readSignLine = do
-    [d, a, b] <- words <$> getLine
-    return (read d + read a, read d - read b)
-
-readSignLines n = unzip <$> Monad.replicateM n readSignLine
-
-readTestCase = getLine >>= readSignLines . read
-
 main = do
-    nTests <- read <$> getLine
-    Monad.forM_ [1..nTests] $ \i -> do
-        (ms, ns) <- readTestCase
-        let (maxSize, count) = solve ms ns
-        putStr $ "Case #" ++ show i ++ ": " ++ show maxSize ++ " " ++ show count ++ "\n"
+  nTests <- read <$> getLine
+  Monad.forM_ [1..nTests] $ \i -> do
+    nSigns <- read <$> getLine
+    signLines <- Monad.replicateM nSigns getLine
+    let signs = zipWith lineToSign [0..] signLines
+        contiguousRanges = signsToContiguousRanges signs ++ signsToContiguousRanges (map reverseSign signs)
+        maxSize = maximum $ map size contiguousRanges
+        count = Set.size $ Set.fromList $ filter ((== maxSize) . size) contiguousRanges
+    putStr $ "Case #" ++ show i ++ ": " ++ show maxSize ++ " " ++ show count ++ "\n"
