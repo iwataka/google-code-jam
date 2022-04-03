188928ac46c8d7d3717deac1ccb8616438cc2c30
iwataka <ap.gr.le@gmail.com>
2022-04-03 14:42:36 +0900

    Solve Test Set 2 in Chain Reactions in 2022 Qualification Round.

--- 2022/qualification_round/chain_reactions.py
+++ 2022/qualification_round/chain_reactions.py
@@ -1,32 +1,29 @@
-import itertools
-
 T = int(input())
 for t in range(T):
     N = int(input())
     Fs = list(map(int, input().split()))
     Ps = list(map(lambda i: i - 1, map(int, input().split())))
-    initiaters = []
-    for i in range(N):
-        if i not in Ps:
-            initiaters.append(i)
-    perms = itertools.permutations(initiaters)
+    initiaters = [i for i in range(N) if i not in Ps]
+    module_count = {i: 0 for i in range(N)}
+    for i in initiaters:
+        module_count[i] += 1
+    for i in Ps:
+        if i != -1:
+            module_count[i] += 1
+    module_stack = initiaters[:]
+    module_points = {i: [0] for i in initiaters}
     result = 0
-    for perm in perms:
-        available = list(range(N))
-        total_fun = 0
-        for i in perm:
-            max_fun = 0
-            index = i
-            while True:
-                if index not in available:
-                    break
-                fun = Fs[index]
-                max_fun = max(max_fun, fun)
-                available.remove(index)
-                if Ps[index] != -1:
-                    index = Ps[index]
-                else:
-                    break
-            total_fun += max_fun
-        result = max(result, total_fun)
+    while module_stack:
+        module = module_stack.pop()
+        points = module_points[module]
+        count = module_count[module]
+        if count == len(points):
+            min_point = min(points)
+            result += sum(points) - min_point
+            next_module = Ps[module]
+            if next_module == -1:
+                result += max(Fs[module], min_point)
+            else:
+                module_points.setdefault(next_module, []).append(max(Fs[module], min_point))
+                module_stack.append(next_module)
     print(f"Case #{t + 1}: {result}")
